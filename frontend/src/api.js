c89509a39f819e8de7df6f9588b76bfc82b642be
K Ramesh <krameshr348@gmail.com>
2026-09-21 10:26:34 +0530

    configure frontend api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,14 @@
-import axios from "axios";const api=axios.create({baseURL:import.meta.env.VITE_API_URL||"http://localhost:5000/api"});export const setToken=t=>t?api.defaults.headers.common.Authorization=`Bearer ${t}`:delete api.defaults.headers.common.Authorization;export default api;
+import axios from "axios";
+
+const api = axios.create({
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://referral-commission-backend.vercel.app/api",
+});
+
+export const setToken = (t) =>
+  t
+    ? (api.defaults.headers.common.Authorization = `Bearer ${t}`)
+    : delete api.defaults.headers.common.Authorization;
+
+export default api;
